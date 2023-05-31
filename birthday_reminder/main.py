d20c532c82af5e137c16abc671144a8d0013e415
Mohammed Haque <mhaque21@terpmail.umd.edu>
2023-05-30 22:39:23 -0400

    can't figure out message.content for server messages

--- birthday_reminder/main.py
+++ birthday_reminder/main.py
@@ -4,7 +4,7 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
-bot = discord.Client(intents=intents)
+bot = discord.Client(intents=discord.Intents.default())
 TOKEN = 'MTExMzEzMTY2NDQyMDM5MzAxMA.Ges2Nz.WRgvJta4FCc-cuBqTFG4xyYJqGzh4U118tHT7k'
 
 @bot.event
@@ -22,9 +22,18 @@ async def on_ready():
     print(f'{bot.user} is now running and in {guild_ct} guilds')
 
 @bot.event
-async def on_message(msg):
-    if msg.content == "hello":
-        await msg.channel.send("hey bro")
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    #msg = msg.lower()
+    
+    print(f'{message.author} said {message.content} in {message.channel}')
 
+    if isinstance(message.channel, discord.channel.DMChannel):
+        await message.author.send(f'Okay I am a bot and have no clue what are you on. But I gotcha you said {message.content}')
+
+    elif (str(message.channel) == "just-for-bot-testing"):
+        #print(f'the type of channel {type(message.channel)}')
+        await message.channel.send(f'You said {message.content}')
 
 bot.run(TOKEN)
