a0669118c2ea653e1daa88ce5c92bc034665b561
Mohammed Haque <mhaque21@terpmail.umd.edu>
2023-06-22 00:55:55 -0400

    insta added

--- notifications/main.py
+++ notifications/main.py
@@ -1,9 +1,8 @@
 import discord
-import re
-import requests
-import os
-import json
 import scrapetube
+import instaloader 
+import re
+
 
 from discord.ext import commands, tasks
 
@@ -19,10 +18,18 @@ intents.moderation = True
 
 TOKEN = "MTExOTg1Mjg1ODg1OTMzNTcxMg.G9lWmN.H4_6ciyxf_oXfar83e1uktlvveVakg4lFvTdZc"
 
+insta_bot = instaloader.Instaloader()
+
+
 latest_lbg = []
 latest_442 = []
-latest_espn = []
+latest_beany = []
 latest_ps = []
+latest_caleb = []
+latest_tifo = []
+latest_fab_romano = []
+latest_livehwg = []
+
 channel_id_skits: str
 channel_id_sports: str
 channel_id_gaming: str
@@ -40,7 +47,7 @@ async def on_ready():
     for guild in bot.guilds:
         print(f'- {guild.id} (name: {guild.name})')
         guild_ct += 1
-        if str(guild.name) == "Homies":
+        if str(guild.name) == "SamShed's server":
             channels = guild.channels
 
     print(f'{bot.user} is now running and in {guild_ct} servers')
@@ -64,13 +71,21 @@ async def on_ready():
 
 @tasks.loop(seconds=120)
 async def send_message():
-    global latest_lbg, latest_espn, latest_442, latest_ps
+    global latest_lbg, latest_caleb, latest_442, latest_ps, latest_beany,latest_tifo
+    global latest_fab_romano, latest_livehwg
     global channel_id_sports, channel_id_skits, channel_id_gaming
+   
+    
     videos_lbg = scrapetube.get_channel("UCWiY6fYdxuEe78r-0uFCnhA")
     videos_442 = scrapetube.get_channel("UC4SUUloEcrgjsxbmy_rQQXA")
-    videos_espn = scrapetube.get_channel("UC6c1z7bA__85CIWZ_jpCK-Q")
+    videos_beany = scrapetube.get_channel("UCiVg6vRhuyjsWgHkDNOig6A")
     videos_ps = scrapetube.get_channel("UC-2Y8dQb0S6DtpxNgAKoJKA")
-    
+    videos_caleb = scrapetube.get_channel("UCI1XS_GkLGDOgf8YLaaXNRA")
+    videos_tifo = scrapetube.get_channel("UC6ZMmQaL9wZYo4iLw8n8xiA")
+    insta_fabr = instaloader.Profile.from_username(insta_bot.context, 'fabriziorom')
+    insta_livehwg= instaloader.Profile.from_username(insta_bot.context, 'liveherewego')
+
+
     for video in videos_lbg:
         if video['videoId'] not in latest_lbg:
             latest_lbg.append(video['videoId'])
@@ -80,6 +95,15 @@ async def send_message():
             await send_message_to_channel(message, channel_id_skits)
         break
 
+    for video in videos_caleb:
+        if video['videoId'] not in latest_caleb:
+            latest_caleb.append(video['videoId'])
+            if len(latest_caleb) > 1:
+                latest_caleb.pop(0)
+            message = f"New Calebcity video:\nhttps://www.youtube.com/watch?v={video['videoId']}&ab_channel=CalebCity \n"
+            await send_message_to_channel(message, channel_id_skits)
+        break
+    
     for video in videos_442:
         if video['videoId'] not in latest_442:
             latest_442.append(video['videoId'])
@@ -89,12 +113,21 @@ async def send_message():
             await send_message_to_channel(message, channel_id_skits)
         break
 
-    for video in videos_espn:
-        if video['videoId'] not in latest_espn:
-            latest_espn.append(video['videoId'])
-            if len(latest_espn) > 1:
-                latest_espn.pop(0)
-            message = f"New ESPN_FC video:\nhttps://www.youtube.com/watch?v={video['videoId']}&ab_channel=ESPNFC \n"
+    for video in videos_beany:
+        if video['videoId'] not in latest_beany:
+            latest_beany.append(video['videoId'])
+            if len(latest_beany) > 1:
+                latest_beany.pop(0)
+            message = f"New BeanymanSports video:\nhttps://www.youtube.com/watch?v={video['videoId']}&ab_channel=BeanymanSports \n"
+            await send_message_to_channel(message, channel_id_sports)
+        break
+
+    for video in videos_tifo:
+        if video['videoId'] not in latest_tifo:
+            latest_tifo.append(video['videoId'])
+            if len(latest_tifo) > 1:
+                latest_tifo.pop(0)
+            message = f"New Tifo IRL video:\nhttps://www.youtube.com/watch?v={video['videoId']}&ab_channel=TifoIRL \n"
             await send_message_to_channel(message, channel_id_sports)
         break
 
@@ -107,6 +140,37 @@ async def send_message():
             await send_message_to_channel(message, channel_id_gaming)
         break
 
+    for post in insta_fabr.get_posts():
+        if post not in latest_fab_romano:
+            latest_fab_romano.append(post)
+            if len(latest_fab_romano) > 1:
+                latest_fab_romano.pop(0)
+
+            match = re.search(r'<Post\s+(\S+)>', str(post))
+            if match:
+                post_id = match.group(1)
+                message = f"Fabrizio Romano\'s new post: https://www.instagram.com/p/{post_id}/?hl=en"
+                await send_message_to_channel(message, channel_id_sports)
+                
+            else:
+                print("error")
+                
+        break
+
+    for post in insta_livehwg.get_posts():
+        if post not in latest_livehwg:
+            latest_livehwg.append(post)
+            if len(latest_livehwg) > 1:
+                latest_livehwg.pop(0)
+
+            match = re.search(r'<Post\s+(\S+)>', str(post))
+            if match:
+                post_id = match.group(1)
+                message = f"Liveherewego posted something new: https://www.instagram.com/p/{post_id}/?hl=en"
+                await send_message_to_channel(message, channel_id_sports)
+            else:
+                print("error")
+        break
 
 async def send_message_to_channel(message, channel_id):
     #global channel_id
