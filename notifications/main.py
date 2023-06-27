9a88b2c660134d8469caea7057733f386f7a213c
Mohammed Haque <mhaque21@terpmail.umd.edu>
2023-06-26 20:59:36 -0400

    chanegd to samshd

--- notifications/main.py
+++ notifications/main.py
@@ -45,7 +45,7 @@ async def on_ready():
     for guild in bot.guilds:
         print(f'- {guild.id} (name: {guild.name})')
         guild_ct += 1
-        if str(guild.name) == "Homies":
+        if str(guild.name) == "SamShed's server":
             channels = guild.channels
 
     print(f'{bot.user} is now running and in {guild_ct} servers')
